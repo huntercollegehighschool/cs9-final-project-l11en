a1b6aa06e54f8d27eb2618cd79cadc38e958ecf8
Maya Green <94643911+l11en@users.noreply.github.com>
2022-01-25 05:02:58 +0000

    project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
-Name(s):
-Name of Project:
+Name(s):Maya Green
+Name of Project:Retail Therapy
 """
 
 #Write the main part of your program here. Use of the other pages is optional.
@@ -9,3 +9,129 @@ Name of Project:
 #import page2  # uncomment if you're using page2
 #import page3  # uncomment if you're using page3
 #import page4  # uncomment if you're using page4
+a1 = input("> You wake up on the floor of what appears to be an empty mall. --Enter any key to continue--")
+a2 = input("\n> A thin layer of water coats the white linoleum floor, but when you stand up, you are completely dry.")
+a3 = input("\n> The only sounds you can hear are a low buzzing noise emanating from the pale strips of LED lights on the ceiling, and the sloshing of water that never seems to touch your feet.")
+name = input("\n> You focus and try to remember as much as you can. Enter your name: ")
+a4 = input("\n> Looking around, you see many empty storefronts.")
+a5 = input("\n> There's one occupied storefront. Strangely, it sports a generic name instead of a brand name: Clothing Store.")
+a6 = input("\n> This store appears to be the only place of interest in this area of the mall. You might as well go into it.")
+a7 = input("\n> It feels like a much longer walk to the Clothing Store than it had looked from where you stood. When you eventually reach the large glass doors, they swing open.")
+a8 = input("\n> Inside the store, two people turn to stare at you.")
+a9 = input("\n> A girl is sitting on a mannequin display. Her long, bubblegum pink hair is tied into pigtails, and she's wearing a matching pink dress. It's very frilly.")
+a10 = input("\n> The other person walks towards you. They're dressed in activewear and sport short brown hair. ...Did those two outfits come from the same store?")
+a11 = input("\n> The person extends a hand.")
+a12 = input("\nLaurel: Hi! I'm Laurel. Great to meet ya!")
+a13 = input("\n"+name+": I'm "+name+".")
+a14 = input("\nLaurel: Did you just get here?")
+a15 = input("\n> You nod your head.")
+a16 = input("\nLaurel: I got here about a day ago, I think!")
+a17 = input("\nLaurel turns towards the pink girl.")
+a18 = input("\nLaurel: Hey, Viscaria, how long have you been here?")
+a19 = input("\nViscaria:...How am I supposed to remember? I don't know, maybe, like, a week or two.")
+a20 = input("\nLaurel: Really? You spent that long just looking at clothes?")
+a21 = input("\nViscaria: What else am I supposed to do? There's literally nothing else here.")
+a22 = input("\nLaurel: I mean, there's gotta be a way out of here, and I'm gonna find it!")
+a23 = input("\nViscaria: And how exactly do you plan to leave this place?") 
+a24 = input("\nLaurel: .....")
+a25 = input("\nLaurel: Well, I'm not sure yet, but I've gathered my exploration gear and I'm ready to find a way out!")
+a26 = input("\nViscaria: I've already explored this place. If you explore, you won't like what you find. It's better to just stay here. Enjoy the free clothes.")
+a27 = input("\nLaurel: You know that just made me more curious, right? Well, I'm gonna go check this place out. You can come with me if you want, "+name+".")
+a28 = input("\nViscaria: Laurel, you have no idea what you're doing. "+name+", don't listen to them. If you really want to know how to leave this place, you can come with me. But, like, seriously- you won't like what you find here.")
+a29 = input("\nLaurel: Well, whatever, I'm going to prove you wrong and find a way out! And when I do, I'm gonna come back for you guys so you can leave with me!")
+b4 = input("\n> Viscaria smiles fondly.")
+b5 = input("\nVicaria: Okay. We'll see you later, then.")
+b6 = input("\n> Laurel picks up their backpack and runs out the door.")
+b7 = input("\nViscaria: Well, since you chose to follow me, I'll show you how to leave here.")
+b8 = input("\n> Viscaria gets up and starts to walk further into the store, beckoning for you to follow her.")
+b9 = input("\n> She leads you to the back of the store and stops before a mirror.")
+b10 = input("\nViscaria: Before I show this to you... I have a question.")
+b11 = input("\nViscaria: My favorite fable is Aesop's \"The Town Mouse and the Country Mouse\". The town mouse lives a life of constant danger, but always has good food to eat. The country mouse lives peacefully, but doesn't have much food.")
+b12 = input("\nViscaria: Would you rather live as the town mouse or the country mouse?")
+choice1 = input("\na. Town Mouse  b. Country Mouse ")
+if choice1 in ['a','A']:
+  c1 = input("\nViscaria: The town mouse, huh... personally, I would pick the country mouse. Anyways, thanks for humoring me. Now, let's go.")
+  c2 = input("\n> Viscaria steps into the mirror, and her body phases through it. Once her face is halfway concealed by the mirror, she reaches her hand out.")
+  c3 = input("\n> You grab it, and she pulls you through to the other side of the mirror.")
+  c4 = input("\n> After falling through the mirror, you stand up. You appear to be in a different area of the mall. There are no storefronts visible, and the layer of water on the floor is thicker. In front of you, there's a tiled fountain.")
+  c5 = input("\n> Viscaria walks towards the fountain and sits on the edge of it. You follow and stand in front of her.")
+  c6 = input("\n> She picks up a penny that sat on the side of the fountain and throws it into the fountain.")
+  c7 = input("\n> After the penny sinks into the water, the figure of a woman rises from it. As she begins to speak, her voice seems to reverberate through the air like crashing waves.")
+  c8 = input("\nFountain Lady: One shall stay, one shall go, both will leave.")
+  c9 = input("\nViscaria: Feel the water.")
+  c10 = input("\n> You dip your fingers into the water. Upon hitting the surface of the water, the tips of your fingers are no longer visible under the surface. You recoil and withdraw your fingers.")
+  c11 = input("\nViscaria: I really like photography, you know. It's super pretty.")
+  c12 = input("\nViscaria: Have you ever seen a photo of someone wearing a dress underwater? The way the fabric floats makes them look like they're suspended in air...")
+  c13 = input("\n> Viscaria gently runs her fingers over her pink chiffon skirt.")
+  c14 = input("\nViscaria: One shall stay, one shall go... whatever that means, I'm okay with staying. You have more ambition than me, so you deserve to leave.")
+  c15 = input("\nViscaria: I bet this dress would look pretty underwater.")
+  c16 = input("\n> Viscaria leans backwards and falls into the fountain, disappearing from view.")
+  choice2 = input("\na. Jump into the fountain  b. Don't jump into the fountain ")
+  if choice2 in ['a','A']:
+    e1 = input("\n> As you plunge into the water, you see that Viscaria is sinking quickly into pitch-black waters.")
+    choice4 = input("\na. Grab her hand  b. Swim back up to the surface ")
+    if choice2 in ['a','A']:
+      f1 = input("\n> After grabbing hold of Viscaria's hand, you struggle to pull her back up towards the surface, but she keeps sinking and pulling you down with her.")
+      f2 = input("\n> As you sink further and further down, you begin to lose consciousness.")
+      f3 = input("\n> Viscaria was right. Her dress looks beautiful underwater.")
+      print("--END-- Stranded Together")
+    elif choice2 in ['b','B']:
+      g1 = input("\n> You break the water's surface, and you're back at the fountain.")
+      g2 = input("\nFountain Lady: Your friend has chosen to stay. Now, it's time for you to leave. I do hope the guilt does not break you!")
+      g3 = input("\n> The Fountain Lady pushes your head back into the water. This time, when you open your eyes, you are lying in your bed, and your hair is wet. Perhaps you had just taken a shower.")
+      print("--END-- Watery Tomb")
+    else:
+      while choice2 not in ['a','A','b','B']:
+        h1 = input("\n> That's not an option.")
+        choice2 = input("\na. Grab her hand  b. Swim back up to the surface ")
+        if choice2 in ['a','A']:
+          f1 = input("\n> After grabbing hold of Viscaria's hand, you struggle to pull her back up towards the surface, but she keeps sinking and pulling you down with her.")
+          f2 = input("\n> As you sink further and further down, you begin to lose consciousness.")
+          f3 = input("\n> Viscaria was right. Her dress looks beautiful underwater.")
+          print("--END-- Stranded Together")
+        elif choice2 in ['b','B']:
+          g1 = input("\n> You break the water's surface, and you're back at the fountain.")
+          g2 = input("\nFountain Lady: Your friend has chosen to stay. Now, it's time for you to leave. I do hope the guilt does not break you!")
+          g3 = input("\n> The Fountain Lady pushes your head back into the water. This time, when you open your eyes, you are lying in your bed, and your hair is wet. Perhaps you had just taken a shower.")
+          print("--END-- Watery Tomb")
+  elif choice2 in ['b','B']:
+    f1 = input("\nO")
+  else:
+    while choice2 not in ['a','A','b','B']:
+     d1 = input("\n> That's not an option.")
+     choice2 = input("\na. Jump into the fountain  b. Don't jump into the fountain ")
+    if choice2 in ['a','A']:
+      e1 = input("\no")
+    elif choice2 in ['b','B']:
+      f1 = input("\nO")
+elif choice1 in ['b','B']:
+  c1 = input("\nViscaria: The country mouse, huh! I would choose the same. Anyways, thanks for humoring me. Now, let's go.")
+  c2 = input("\n> Viscaria steps into the mirror, and her body phases through it. Once her face is halfway concealed by the mirror, she reaches her hand out.")
+  c3 = input("\n> You grab it, and she pulls you through to the other side of the mirror.")
+  c4 = input("\n> After falling through the mirror, you stand up. You appear to be in a different area of the mall. There are no storefronts visible, and the layer of water on the floor is thicker. In front of you, there's a tiled fountain.")
+  c5 = input("\n> Viscaria walks towards the fountain and sits on the edge of it. You follow and stand in front of her.")
+  c6 = input("\n> She picks up a penny that sat on the side of the fountain and throws it into the fountain.")
+  c7 = input("\n> After the penny sinks into the water, the figure of a woman rises from it. As she begins to speak, her voice seems to reverberate through the air like crashing waves.")
+  c8 = input("\nFountain Lady: One shall stay, one shall go, both will leave.")
+  c9 = input("\nViscaria: Feel the water.")
+  c10 = input("\n> You dip your fingers into the water. Upon hitting the surface of the water, the tips of your fingers are no longer visible under the surface. You recoil and withdraw your fingers.")
+  c11 = input("\nViscaria: Since you also would choose the country mouse, that means you would prefer a peaceful but hungry existence over a dangerous but satisfactory one, right? How about we just stay here instead of trying to leave?")
+  choice2 = input("\na. I want to stay  b. I want to leave ")
+  if choice2 in ['a','A']:
+    print("--END-- Viscaria Defeated/Bitter Victory")
+  elif choice2 in ['b','B']:
+      print("--END-- Together Forever and Never to Part/Viscaria Alone")
+else:
+  while choice1 not in ['a','A','b','B']:
+    d1 = input("\n> That's not an option.")
+    choice1 = input("\na. Town Mouse  b. Country Mouse ")
+    if choice1 in ['a','A']:
+      e1 = input("\no")
+    elif choice1 in ['b','B']:
+      f1 = input("\nO")
+
+
+
+
+
+
